c24aca5aeb90d42e04c4a8b2edb20fb7307db119
Pablo <pabloagujeronegro2013@gmail.com>
2025-12-24 08:06:04 +0100

    Init proyect

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+from pathlib import Path
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8412514934:AAEWziOaCr6-wtNMrBB-A37buX_iZvEeGK4"
+
+
+async def villancico(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    ruta = Path("files/")  # Cambia por tu carpeta
+    archivos_ogg = [f for f in ruta.iterdir() if f.is_file() and f.suffix == ".ogg"]
+    file = random.choice(archivos_ogg)
+    with open(file, "rb") as voice:
+        await update.message.reply_voice(voice)
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    mensaje = """
+🤖 *Comandos disponibles*:
+/start - Inicia el bot
+/help - Muestra este mensaje
+/villancico - Recibe una nota de voz con un villancico
+"""
+    # Envía el mensaje en formato Markdown
+    await update.message.reply_text(mensaje, parse_mode="Markdown")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("¡Hola! Bienvenido al bot 🎄")
+    # Ejecutar help automáticamente
+    await help_command(update, context)
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("villancico", villancico))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_command))
+app.run_polling()
